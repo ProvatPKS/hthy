b190d78fc60290ed1ae4c5af43a3db77d54745b0
ProvatPKS <38600681+ProvatPKS@users.noreply.github.com>
2019-08-30 20:20:53 +0530

    Create JbViewport.js

---
+++ JobBot/src/main/resources/static/js/view/JbViewport.js
@@ -0,0 +1,42 @@
+Ext.require([ 'Ext.container.Viewport', 'Ext.panel.Panel', 'Ext.tab.Panel', 'Ext.container.Container', 'Ext.grid.*' ]);
+
+Ext.define('JobBot.view.JbViewport', {
+	extend: 'Ext.container.Viewport',
+	id: 'fxViewportId',
+	alias: 'widget.jbViewport',
+
+	layout: {
+		type: 'border',
+		splitter: false
+	},
+	defaults: {},
+	initComponent: function() {
+		var me = this;
+		var config = {
+			items: [ {
+				region: 'north',
+				height: 56,
+				hidden: true
+				//xtype: 'fxHeader'
+
+			}, {
+				region: 'center',
+				xtype: 'jblogin'
+			}, {
+				xtype: 'panel',
+				height: 20,
+				border: 0,
+                hidden: true,
+				region: 'south'
+			} ]
+		};
+		Ext.applyIf(me, config);
+		me.callParent(arguments);
+	},
+	listeners: {
+		afterrender: function(viewport, eopts) {
+			console.log('view port ->after render :');
+		}
+	}
+
+});
